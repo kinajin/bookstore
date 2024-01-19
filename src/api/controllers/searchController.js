3b8bf5691963d1080d1bf768a3128d1b3d271a00
kinajin <kinajin22@gmail.com>
2024-01-20 01:23:46 +0900

    feature:검색 기능 수정(attribute 제한, 좋아요수 대표이미지수 추가, totalcount totalpage currentpage 페이지네이션 정보 추가)

--- src/api/controllers/searchController.js
+++ src/api/controllers/searchController.js
@@ -5,8 +5,8 @@ exports.searchBooks = async (req, res) => {
   let { category, keyword, page, limit } = req.query;
 
   // 디폴드 값 설정 또는 숫자로 전환
-  page = parseInt(page) || 1;
-  limit = parseInt(limit) || 8;
+  page = parseInt(page) || 1; // 현재 페이지
+  limit = parseInt(limit) || 8; // 페이지당 최대 개수
 
   // 보여주는 시작점 설정
   const offset = (page - 1) * limit;
@@ -16,6 +16,7 @@ exports.searchBooks = async (req, res) => {
 
   // 카테고리가 제공되면 검색 조건에 추가
   let categoryConditions = {};
+
   if (category) {
     categoryConditions.category = category;
   }
@@ -40,12 +41,47 @@ exports.searchBooks = async (req, res) => {
   try {
     const newBooks = await db.Books.findAll({
       where: searchConditions,
-      offset: offset,
-      limit: limit,
+      attributes: [
+        "id",
+        "Title",
+        "Author",
+        "Summary",
+        "Price",
+        "PrimaryImageID",
+        [
+          db.sequelize.literal(
+            "(SELECT COUNT(*) FROM Likes WHERE Likes.BookID = Books.id)",
+          ),
+          "LikesCount",
+        ],
+      ],
+      include: [
+        {
+          model: db.BookImages,
+          attributes: ["ImageURL"],
+          where: { id: db.Sequelize.col("Books.PrimaryImageID") },
+          required: false,
+        },
+      ],
+      offset, // 페이지에 따른 시작 위치
+      limit, // 한 페이지에 표시할 항목의 수
     });
 
+    // 총 결과 개수
+    const totalCount = newBooks.length;
+
+    // 전체 페이지 수
+    const totalPages = Math.ceil(totalCount / limit);
+
     if (newBooks.length > 0) {
-      res.json({ newBooks });
+      res.json({
+        newBooks,
+        pagination: {
+          totalItems: totalCount,
+          totalPages: totalPages,
+          currentPage: page,
+        },
+      });
     } else {
       res.status(404).json({ message: "해당 검색의 도서가 없습니다" });
     }
